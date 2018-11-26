c18ebab48265675334b11b07da8719af85562226
Virtual1 <jessica@virtualangels.co.za>
2018-11-26 10:29:27 +0200

    Updated template to loop through items, still static data though S7L40

--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Listings
+
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listings.objects.all()
+
+    context = {
+        'listings': listings
+    }
+
+    return render(request, 'listings/listings.html', context)
+
 def listing(request):
     return render(request, 'listings/listing.html')
+
 def search(request):
     return render(request, 'listings/search.html')
