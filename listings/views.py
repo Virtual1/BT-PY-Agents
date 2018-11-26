980f502b3b3bb6031311618aa44f0557103dcb39
Virtual1 <jessica@virtualangels.co.za>
2018-11-26 15:49:58 +0200

    Single Listing page - dynamic - created S7L44

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listings
+
 
 def index(request):
     listings = Listings.objects.order_by('-list_date').filter(is_published=True)
@@ -16,7 +17,13 @@ def index(request):
     return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listings, pk=listing_id)
+
+    context = {
+        'listing': listing
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')
