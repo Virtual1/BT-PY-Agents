101a55aa1d8f20ec46ac01c0acc8425bf04eae33
Virtual1 <jessica@virtualangels.co.za>
2018-12-10 16:25:47 +0200

    Done S9L57

--- V1Agents/urls.py
+++ V1Agents/urls.py
@@ -27,4 +27,6 @@ urlpatterns = [
 
     path('accounts/', include('accounts.urls')),
 
+    path('contacts/', include('contacts.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
