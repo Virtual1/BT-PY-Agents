d18cdb18cb726817067ad51e5132637f6c8a9c4e
Virtual1 <jessica@virtualangels.co.za>
2018-12-10 19:37:29 +0200

    S9L59 done

--- V1Agents/settings.py
+++ V1Agents/settings.py
@@ -141,3 +141,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+#Email config
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'WeWorkGermiston@gmail.com'
+EMAIL_HOST_PASSWORD = 'VA_W3W0rk)(*'
+EMAIL_USE_TLS = True
